0035f613501ee5937ee00eb04cc6aed101a41fae
tmkkk <tymenty33@gmail.com>
2020-01-15 14:04:12 +0100

    :ambulance: Teraz wszystko działa.

--- 25_powtorzenie/drzewa_expr/rozwiazanie.ml
+++ 25_powtorzenie/drzewa_expr/rozwiazanie.ml
@@ -1,32 +1,4 @@
-(* Zwraca rozkład [p_1 ^ a_1, ..., p_k ^ a_k] liczby n *)
-let factorize n = 
-    let factors = ref []
-    and n = ref n
-    and d = ref 2 in
-    while !d * !d <= !n do
-        if !n mod !d = 0 then
-        begin
-            let div = ref 1 in
-            while !n mod !d = 0 do
-                div := !div * !d;
-                n := !n / !d
-            done;
-            factors := !div :: !factors
-        end;
-        d := !d + 1
-    done;
-    if !n <> 1 then
-        factors := !n :: !factors;
-    !factors;;
-
-let rec gcd a b = 
-    if b = 0 then
-        a
-    else
-        gcd b (a mod b);;
-
-let lcm a b =
-    a * b / gcd a b;;
+open Utils;;
 
 type expr =
     NWD of expr * expr |
@@ -34,11 +6,11 @@ type expr =
     Number of int;;
 
 let wstaw (e, n) =
-    let rec evaluate n = function
+    let rec evaluate n t = 
+        match t with
         | NWD (p, q) -> gcd (evaluate n p) (evaluate n q)
-        | NWW (p, q) -> lcm (evaluate n p) (evaluate n p)
-        | Number x ->
-            if x = 0 then n else x in
+        | NWW (p, q) -> lcm (evaluate n p) (evaluate n q)
+        | Number x -> if x = 0 then n else x in
     let res_1 = evaluate 1 e
     and res_n = evaluate n e in
     if (res_n mod res_1 = 0) && (res_n = n) then
